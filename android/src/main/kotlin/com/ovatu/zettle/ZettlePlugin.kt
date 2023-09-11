ba93dd6a7af9f5fa94b2876e6505a539c93cac69
stepan-bezhuk-axon <sbezhuk@axon.dev>
2023-09-11 18:54:21 +0300

    fix: amount sum on android

--- android/src/main/kotlin/com/ovatu/zettle/ZettlePlugin.kt
+++ android/src/main/kotlin/com/ovatu/zettle/ZettlePlugin.kt
@@ -216,7 +216,7 @@ class ZettlePlugin: FlutterPlugin, MethodCallHandler, ActivityAware, PluginRegis
     val reference = TransactionReference.Builder(internalUniqueTraceId).build()
 
     val intent = CardPaymentActivity.IntentBuilder(activity)
-            .amount(args["amount"] as Double)
+            .amount((((args["amount"] as Double) * 100).toInt()).toLong())
             .reference(reference)
             .enableLogin(args["enableLogin"] as Boolean? ?: true)
             .enableTipping(args["enableTipping"] as Boolean? ?: true)
